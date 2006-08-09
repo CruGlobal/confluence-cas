b8aa4fb4360e1dbf941734f660afb23ae2138b95
matt.drees <matt.drees@ccci.org>
2006-08-09 21:43:50 +0000

    finished uscm code

--- src/org/uscm/confluence/cas/UscmConfluenceCasAuthenticator.java
+++ src/org/uscm/confluence/cas/UscmConfluenceCasAuthenticator.java
@@ -13,39 +13,46 @@ import com.atlassian.confluence.user.UserAccessor;
 public class UscmConfluenceCasAuthenticator extends ConfluenceCasAuthenticator {
   private static final long serialVersionUID = 1L;
 
-  private static Logger log = Logger.getLogger(UscmConfluenceCasAuthenticator.class);
-  
+  private static Logger log = Logger
+      .getLogger(UscmConfluenceCasAuthenticator.class);
+
   @SuppressWarnings("unchecked")
   @Override
   protected Principal getUser(Assertion assertion) {
     Principal user = null;
     String username = assertion.getPrincipal().getId();
-    if (username != null) {
-      user = getUser(username);
+    Map<String, String> attributes = assertion.getAttributes();
+    String ssoGuid = attributes.get("ssoGuid");
+
+    if (ssoGuid != null) {
+      user = getUser(ssoGuid);
       if (user == null) {
-        Map<String, String> attributes = assertion.getAttributes();
-        
+
         String[] groups;
-        if (attributes.get("emplid") != null && !attributes.get("emplid").equals("") ) {
+        if (attributes.get("emplid") != null
+            && !attributes.get("emplid").equals("")) {
           groups = new String[2];
           groups[1] = "ccc-employee";
         } else {
           groups = new String[1];
         }
         groups[0] = "confluence-users";
-        
+
         UserAccessor userAccessor = getUserAccessor();
-        userAccessor.addUser(attributes.get("ssoGuid"), 
+        
+        userAccessor.addUser(ssoGuid, 
             String.valueOf(new Date().getTime()),
-            username, 
-            attributes.get("firstName") + " " + attributes.get("lastName"),
-            groups);
+            username, attributes.get("firstName") + " "
+                + attributes.get("lastName"), groups);
+        
+        user = getUser(ssoGuid);
+        if (user == null) {
+          log.error("Added user " + ssoGuid + ", but failed to find it!");
+        }
       }
     } else {
-      log.error("CAS Assertion contained null principal!");
+      log.error("CAS Assertion contained no ssoGuid!");
     }
     return user;
-}
-
-  
+  }
 }
